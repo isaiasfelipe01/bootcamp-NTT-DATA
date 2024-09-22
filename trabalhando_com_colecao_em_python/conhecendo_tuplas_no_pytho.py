89efd31206e52764a5e64ca84799d5d73afcc423
isaiasfelipe01 <isaiasfelipe232@gmail.com>
2024-09-22 14:21:49 -0300

    Conhecendo tuplas no Python

---
+++ trabalhando_com_colecao_em_python/conhecendo_tuplas_no_pytho.py
@@ -0,0 +1,25 @@
+frutas = ('banana', 'laranja', 'abacaxi', 'melancia', 'uva',)
+print(frutas)
+print(frutas[0])
+print(frutas[-1])
+
+letras = tuple("python")
+print(letras)
+
+numeros = tuple(range(10))
+print(numeros)
+
+pais = ("Brasil",)
+print(pais)
+
+matriz = (
+    (1, "a", 3),
+    ("b", 5, 6),
+    (7, 8, "c")
+)
+print(matriz[0])
+
+print(letras[1:4])
+print(letras[:3])
+print(letras[2:])
+print(letras[2::2])
